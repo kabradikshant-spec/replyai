afc7123e603f022f6753d03d43a14629868274f5
kabradikshant-spec <kabradikshant@gmail.com>
2026-04-06 23:58:45 +0530

    fix next config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,4 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
 })
 
-module.exports = withPWA({
-  experimental: {
-    appDir: true,
-  },
-})
+module.exports = withPWA({})
